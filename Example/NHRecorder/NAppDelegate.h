cd5114b241ce08d029d9736ca87bf2f33a7b2a24
Naithar <devias.naith@gmail.com>
2015-06-04 19:16:41 +0300

    Initial commit

---
+++ Example/NHRecorder/NAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NAppDelegate.h
+//  NHRecorder
+//
+//  Created by CocoaPods on 06/04/2015.
+//  Copyright (c) 2014 Naithar. All rights reserved.
+//
+
+@import UIKit;
+
+@interface NAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
